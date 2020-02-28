f1dfc6e514d866048cc4cbef0524c8ffdd072cfb
jalvarezbretana <jalvarezbretana@danielcastelao.org>
2020-02-28 14:43:12 +0100

    File

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -12,7 +12,7 @@ public class Principal {
         //Pido la descripcion del repositorio
         String descRepo = JOptionPane.showInputDialog("Introduce la descripcion");
         //Metodo para crear el repositorio usando el archivo property (.github)
-        GitHub github = GitHubBuilder.fromPropertyFile("/home/local/DANIELCASTELAO/irodriguezsteuerberg/IdeaProjects/PluginNetbeans/.github").build();
+        GitHub github = GitHubBuilder.fromPropertyFile("/home/local/DANIELCASTELAO/jalvarezbretana/IdeaProjects/Github/.github").build();
         GHRepository repo = github.createRepository(nombreRepo,descRepo,
                 "https://www.google.com/",false);
         //Añado a damian a los colaboradores
