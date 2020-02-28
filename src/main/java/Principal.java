3d44f9f6e630f58db7edf037c103211a084541c8
jalvarezbretana <jalvarezbretana@danielcastelao.org>
2020-02-28 14:33:58 +0100

    File

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -7,14 +7,12 @@ import java.io.IOException;
 
 public class Principal {
     public static void main(String[] args) throws IOException {
-        //Pido el token para poder crear el repositorio
-        String token = JOptionPane.showInputDialog("Introduce el token");
         //Pido el nombre del repositorio que voy a crear
         String nombreRepo = JOptionPane.showInputDialog("Introduce el nombre del repositorio");
         //Pido la descripcion del repositorio
         String descRepo = JOptionPane.showInputDialog("Introduce la descripcion");
-        //Metodo para crear el repositorio usando un token
-        GitHub github = new GitHubBuilder().withJwtToken(token).build();
+        //Metodo para crear el repositorio usando el archivo property (.github)
+        GitHub github = GitHubBuilder.fromPropertyFile("/home/local/DANIELCASTELAO/irodriguezsteuerberg/IdeaProjects/PluginNetbeans/.github").build();
         GHRepository repo = github.createRepository(nombreRepo,descRepo,
                 "https://www.google.com/",false);
         //Añado a damian a los colaboradores
