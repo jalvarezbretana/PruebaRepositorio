3bfb339a44c99b6407b81b076120a0f0d9966fb7
jalvarezbretana <jalvarezbretana@danielcastelao.org>
2020-03-11 14:32:58 +0100

    Ejercicio de crear y clonar

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,12 +1,15 @@
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 
 import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
 
 public class Principal {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, GitAPIException {
         //Pido el nombre del repositorio que voy a crear
         String nombreRepo = JOptionPane.showInputDialog("Introduce el nombre del repositorio");
         //Pido la descripcion del repositorio
@@ -15,6 +18,13 @@ public class Principal {
         GitHub github = GitHubBuilder.fromPropertyFile("/home/local/DANIELCASTELAO/jalvarezbretana/IdeaProjects/Github/.github").build();
         GHRepository repo = github.createRepository(nombreRepo,descRepo,
                 "https://www.google.com/",false);
+
+        //
+        Git git = Git.cloneRepository()
+                .setURI( "https://github.com/eclipse/jgit.git" )
+                .setDirectory(new File("https://github.com/jalvarezbretana/basico-master.git"))
+                .call();
+
         //Añado a damian a los colaboradores
         repo.addCollaborators(github.getUser("damiancastelao"));
     }
